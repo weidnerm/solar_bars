ccbe6b085d927c2721a345cf31b1e2bb77deb169
Michael Weidner <michael.weidner@motorolasolutions.com>
2018-10-14 12:34:39 -0400

    hide action bar. add section titles

--- app/src/main/java/com/example/mike/solarbars/MainActivity.java
+++ app/src/main/java/com/example/mike/solarbars/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -65,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mChart = (BarChart) findViewById(R.id.bargraph);
